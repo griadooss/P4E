c0cad7e26163b1166c45bfc1d3dc094da5c9438e
griadooss <github@zimpics.com>
2014-06-21 15:20:38 +1000

    Update raw_imput.c

--- raw_imput.c
+++ raw_imput.c
@@ -3,7 +3,7 @@
   *      Author: griadooss
   *
   * 2.2 Write a program that uses raw_input to prompt a user for their name and then welcomes them. Note that raw_input will pop a dialog box.
-  * Enter Sarah in the pop-up box when you are prompted so your output will match the desired output below.
+  * Enter Sarah in the pop-up box when you are prompted so your output will match the desired output not shown below.
   *
   */
 
