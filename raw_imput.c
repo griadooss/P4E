81cf63bf50ccee7d111e02d980e9fd1d68ed13cc
griadooss <github@zimpics.com>
2014-06-21 15:36:22 +1000

    edited via Eclipse by linking as new source file

--- raw_imput.c
+++ raw_imput.c
@@ -4,6 +4,7 @@
   *
   * 2.2 Write a program that uses raw_input to prompt a user for their name and then welcomes them. Note that raw_input will pop a dialog box.
   * Enter Sarah in the pop-up box when you are prompted so your output will match the desired output not shown below.
+  * Now edited in Eclipse by linking the file through and existing cpp Eclipse Project
   *
   */
 
@@ -13,7 +14,7 @@ enum { max_length = 127 };
 static char sName[max_length];
 
 int main( int argc, char ** argv ) {
-	printf("Enter your first name : ");
+	printf("Enter your first name followed by you second name : ");
 	fflush(stdout);
 	fgets(sName, max_length, stdin);
 	printf("Hello, %s", sName);
