97d1fa1fca65c3965bddadb4346d985c5240b543
griadooss <github@zimpics.com>
2014-06-23 20:11:59 +1000

    cpp version

---
+++ wk01/hello.cpp
@@ -0,0 +1,17 @@
+/*
+ * hello.cpp
+ *
+ *  Created on: 23/06/2014
+ *      Author: griadooss
+ */
+
+#include <iostream>
+using namespace std;
+
+int main( int argc, char ** argv ) {
+	cout << "Hello, World!";
+	return 0;
+}
+
+
+
