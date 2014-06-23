34b8b9d2b64a970ccb9f1fbc739143953d16b832
griadooss <github@zimpics.com>
2014-06-23 22:01:54 +1000

    java and cpp versions

---
+++ wk02/Raw_input.java
@@ -0,0 +1,14 @@
+// 2.2 Write a program to prompt a user for their name and then welcome them. 
+
+import java.io.*;
+
+public class Raw_input {
+	
+	public static void main(String args[]) throws IOException {
+		System.out.print("Enter your name: ");
+		InputStreamReader inp = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(inp);
+		String name = br.readLine();
+		System.out.println("Hello "+name);
+	}
+}
---
+++ wk02/raw_input.cpp
@@ -0,0 +1,16 @@
+/*
+ * raw_input.cpp
+ *
+ *  Created on: 23/06/2014
+ *      Author: griadooss
+ */
+
+#include <iostream>
+using namespace std;
+
+int main( int argc,  char ** argv ) {
+	string name;
+	cout << "Enter your name : " << flush;
+	cin >> name;
+	cout << "Hello, " << name << endl;
+}
