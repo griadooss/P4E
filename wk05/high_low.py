f92976254b7fa65b9f4428c01a38b69b13b0177e
griadooss <github@zimpics.com>
2014-07-09 11:21:29 +1000

    update and new work

--- wk05/high_low.py
+++ wk05/high_low.py
@@ -1,3 +1,4 @@
+# Assignment 5.2 by griadooss
 
 #5.2 Write a program that repeatedly prompts a user for integer numbers until the user enters 'done'. 
 #    Once 'done' is entered, print out the largest and smallest of the numbers. 
@@ -5,33 +6,33 @@
 #    and put out an appropriate message and ignore the number. 
 #    Enter the numbers from the book for problem 5.1 and Match the desired output as shown.
 
+# Initialize variables
+iMin = None
+iMax = 0
+inp = str("mt") #User input
 
-iSml = None
-iLrg = 0
-UserInput = 0
-iErr=0
-
-while UserInput != "done":
-    UserInput = raw_input("Enter an integer (or "'"done"'" if finished):")
+while inp != "done":
+    inp = raw_input("Enter an integer, or "'"done"'" to finish: ")
     try:
         i = 0 #just a placeholder to test for valid input
-        i = int(UserInput) #test for valid integer input
-        #If the first time through this loop initialize smallest number (iSml) 
-        if iSml == None:
-            iSml = UserInput
-        elif UserInput < iSml: 
-            iSml = UserInput
-        if UserInput > iLrg:
-            iLrg = UserInput
+        i = int(inp) #test for valid integer input
+        #If the first time through this loop then must initialize smallest number (iMin) 
+        if iMin == None:
+            iMin = inp
+        elif inp < iMin: 
+            iMin = inp
+        # iMax is in its own if statement in case only one number is entered, so that it will be both min and max
+        if inp > iMax:
+            iMax = inp
     except:
-        iErr=-1
-        break # This is for when user enters characters other than 'done' .. ie invalid input
+        if inp != "done": #Which will occur if invalid data is entered (other than the 'reserved word' "done"!
+#            print "Try again ... whole numbers only please!"
+            print "Invalid input!"
+        continue
 
-if UserInput != "done":
-    print "Try again ... whole numbers only please!"
-elif iSml is not None: 
-    print "The largest number you entered was : ", iLrg
-    print "The lowest number you entered was : ", iSml
-    print "All done now!"  
+if iMin is not None: 
+    print "Maximum is : ", iMax
+    print "Minimum is : ", iMin
+#    print "All done now!"  
 else:
-    print "All done now!"    
+    print "You aborted before entering any numbers!"    
